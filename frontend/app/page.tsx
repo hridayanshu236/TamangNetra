ddc5e5a5ef5d140e1c303ab46a0d1f10ef2acb70
Hridayanshu Raj Acharya <148516090+hridayanshu236@users.noreply.github.com>
2026-05-02 11:25:06 +0545

    minor fix

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -14,6 +14,7 @@ import {
   UploadCloud,
   FileText,
   X,
+  Plus,
 } from "lucide-react";
 import { Navbar } from "@/src/components/tamangnetra/Navbar";
 import { Badge } from "@/src/components/ui/badge";
